d25f5de3012be9248cec5d6a428013a36676acb3
Nordup <nordup.ondr@gmail.com>
2024-03-07 09:18:46 +0400

    speaking indicator

--- ui/player_panel.gd
+++ ui/player_panel.gd
@@ -2,6 +2,7 @@ extends Control
 class_name PlayerPanel
 
 @export var volume_slider: HSlider
+@export var speaking_indicator: Control
 @export var user_data_events: UserDataEvents
 
 var user_data: UserData
@@ -9,11 +10,17 @@ var user_data: UserData
 
 func _ready() -> void:
 	volume_slider.value_changed.connect(send_volume_changed)
-
-
-func set_user_data(_user_data: UserData) -> void:
-	user_data = _user_data
+	speaking_indicator.visible = false
 
 
 func send_volume_changed(volume: float) -> void:
 	user_data_events.user_volume_changed_emit(user_data.id, volume)
+
+
+func set_user_data(_user_data: UserData) -> void:
+	user_data = _user_data
+	user_data.speaking_changed.connect(speaking_changed)
+
+
+func speaking_changed(speaking: bool) -> void:
+	speaking_indicator.visible = speaking
--- user_data/user_data.gd
+++ user_data/user_data.gd
@@ -1,4 +1,16 @@
 extends Node
 class_name UserData
 
+signal speaking_changed(speaking: bool)
+
+@export var speaking: bool:
+	set(value):
+		speaking = value
+		speaking_changed.emit(value)
+
 var id: int
+
+
+func _process(_delta: float) -> void:
+	if not is_multiplayer_authority(): return
+	speaking = Input.is_action_pressed("speak")

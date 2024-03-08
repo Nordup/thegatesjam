c11e29f73f6153bf735578a760756fac022c533e
Nordup <nordup.ondr@gmail.com>
2024-03-09 02:08:38 +0400

    portal

--- levels/portal/portal.gd
+++ levels/portal/portal.gd
@@ -15,4 +15,5 @@ func _on_portal_entered(body):
 func shut_down(_body) -> void:
 	$Portal/MeshInstance3D.visible = false
 	$Portal/MeshInstance3D2.visible = false
+	$Portal/OmniLight3D.visible = false
 	$Portal/CollisionShape3D.set_deferred("disabled", true)
